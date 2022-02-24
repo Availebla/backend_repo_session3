3d8498e0b154a05c212eb0be7756aee4eeed1b96
Mathew Pius <mathewpius@hotmail.com>
2022-02-24 12:18:31 +0800

    Added javascript files

---
+++ main.js
@@ -0,0 +1,27 @@
+//import the express library
+const express = require ("express") 
+
+//import router from routers.js
+const {router} = require("./routers");
+
+//define server instance
+let app = express();
+//parse using json
+app.use(express.json()); 
+
+//tell the app to use router object for API Mappings
+app.use(router); 
+
+//to start the server at port 3000
+app.listen(3000, (errors) => { 
+    if (errors) {
+        console.log(errors);
+    } else {
+        console.log("Server started on port 3000")
+    }
+});
+
+//(b4 routers.js) go to browser type http://localhost:3000 to see "cannot Get/"
+//Note: server will keep running until you type "Ctrl + C" into the terminal window
+//(after routers.js), http://localhost:3000/home, will show "Welcome..."
+//http://localhost:3000/sum?a=2&b=4 will give you "Sum is: 24" -> concatenation as it recognized params as strings instead of numbers
---
+++ routers.js
@@ -0,0 +1,53 @@
+//To import express library
+const express = require("express");
+
+//Import data.js
+const database = require("./data");
+
+//define a router object
+let router = express.Router();
+
+// define an API to return all the users
+router.get("/users/all", (request, response) => {
+    let users = database.get_all_users();
+    response.send(users);
+});
+
+//define an API to get user by user id
+router.get("/users/by-uid", (request, response) => {
+    let users = database.get_user_by_user_id(request.query.user_id);
+    response.send(users);
+});
+
+// define a POST API to add a new user to database. User's information is passed in the request's body.
+// This is a temporary post -you can see the new added users w /users/all as long as the server is running
+// The info is stored in a temporary file on your RAM and will reset once server is stopped (MySQL for database permanence)
+router.post("/users/add",(request, response) => {
+    //fetch user details from request.body
+    let user = request.body;
+    //add user to database
+    database.add_user(user);
+    //send the success message
+    response.send("User added!");
+});
+
+// // define a GET API with path "/home"
+// router.get("/home", (request, response) => {
+//     response.send("Welcome to Dev Toolkit 2!");
+
+// });
+
+// //define another GET API with path "/sum"
+// router.get("/sum", (request, response) => {
+//     let sum = request.query.a + request.query.b; // in my API request query section will contain params a & b
+//     response.send("Sum is: " + sum);
+// });
+
+// //define a POST API with path "/sum" (Different API even though path is the same)
+// //To run this POST API use POSTMAN (ss on OneNote)
+// router.post("/sum",(request, response) => {
+//     let sum = request.body.a + request.body.b;
+//     response.send("Sum is: " + sum);
+// })
+
+module.exports = {router}; //To export router to the main.js file
