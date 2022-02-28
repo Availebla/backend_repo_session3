adeba378628090ea68d0e2e5c94ee0df1fdca8ea
Mathew Pius <mathewpius@hotmail.com>
2022-02-28 13:09:27 +0800

    as

--- routers.js
+++ routers.js
@@ -31,23 +31,23 @@ router.post("/users/add",(request, response) => {
     response.send("User added!");
 });
 
-// // define a GET API with path "/home"
-// router.get("/home", (request, response) => {
-//     response.send("Welcome to Dev Toolkit 2!");
+// define a GET API with path "/home"
+router.get("/home", (request, response) => {
+    response.send("Welcome to Dev Toolkit 2!");
 
-// });
+});
 
-// //define another GET API with path "/sum"
-// router.get("/sum", (request, response) => {
-//     let sum = request.query.a + request.query.b; // in my API request query section will contain params a & b
-//     response.send("Sum is: " + sum);
-// });
+//define another GET API with path "/sum"
+router.get("/sum", (request, response) => {
+    let sum = request.query.a + request.query.b; // in my API request query section will contain params a & b
+    response.send("Sum is: " + sum);
+});
 
-// //define a POST API with path "/sum" (Different API even though path is the same)
-// //To run this POST API use POSTMAN (ss on OneNote)
-// router.post("/sum",(request, response) => {
-//     let sum = request.body.a + request.body.b;
-//     response.send("Sum is: " + sum);
-// })
+//define a POST API with path "/sum" (Different API even though path is the same)
+//To run this POST API use POSTMAN (ss on OneNote)
+router.post("/sum",(request, response) => {
+    let sum = request.body.a + request.body.b;
+    response.send("Sum is: " + sum);
+})
 
 module.exports = {router}; //To export router to the main.js file
